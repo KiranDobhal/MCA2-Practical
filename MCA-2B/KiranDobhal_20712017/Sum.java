106dee33116bee60dd9b17f712fcebb751e88258
KiranDobhal <85729877+KiranDobhal@users.noreply.github.com>
2021-06-18 13:53:11 +0530

    Add files via upload

---
+++ MCA-2B/KiranDobhal_20712017/Sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Sum
+{  
+ public static void main(String args[]){  
+  Scanner sc=new Scanner(System.in);
+  int i,fact=1;  
+  System.out.println("Enter first number : ");
+  int n1=sc.nextInt();   
+  System.out.println("Enter first number : ");
+  int n2=sc.nextInt();   
+  System.out.println("Enter first number : ");
+  int n3=sc.nextInt();   
+  int s=n1+n2+n3;  
+  System.out.println("Sum of 3 integers is: "+s);    
+ }  
+}  
